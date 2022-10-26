f1e7001b52e86539169ed513674c42077e679507
Gabriele <g.zulini@itsrizzoli.it>
2022-10-26 09:57:13 +0200

    imgs added

--- src/app/prodotto.service.ts
+++ src/app/prodotto.service.ts
@@ -7,12 +7,12 @@ import { Prodotto } from './prodotto.model';
 export class ProdottoService {
 
   private _prodotti :Prodotto[] = [
-    {codice:"001",nome:"prodotto 1", prezzo:100,descrizione:"",foto:""},
-    {codice:"002",nome:"prodotto 2", prezzo:200,descrizione:"",foto:""},
-    {codice:"003",nome:"prodotto 3", prezzo:300,descrizione:"",foto:""},
-    {codice:"004",nome:"prodotto 4", prezzo:400,descrizione:"",foto:""},
-    {codice:"005",nome:"prodotto 5", prezzo:500,descrizione:"",foto:""},
-    {codice:"006",nome:"prodotto 6", prezzo:250,descrizione:"",foto:""},
+    {codice:"001",nome:"prodotto 1", prezzo:100,descrizione:"",foto:"../assets/jordan-4-retro-travis-scott-cactus-jack.webp"},
+    {codice:"002",nome:"prodotto 2", prezzo:200,descrizione:"",foto:"../assets/yeezy-700-wave-runner.jpg"},
+    {codice:"003",nome:"prodotto 3", prezzo:300,descrizione:"",foto:"../assets/yeezy-350-v2-black-red.webp"},
+    {codice:"004",nome:"prodotto 4", prezzo:400,descrizione:"",foto:"../assets/yeezy-350-white-cream.jpg"},
+    {codice:"005",nome:"prodotto 5", prezzo:500,descrizione:"",foto:"../assets/nike-dunk-low-usc.webp"},
+    {codice:"006",nome:"prodotto 6", prezzo:250,descrizione:"",foto:"../assets/nike-off-white-dunk -low-chronicles.jpg"},
   ]
 
   constructor() { }
